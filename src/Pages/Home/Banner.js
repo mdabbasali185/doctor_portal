06bbaf4419ef6e1df20cc90f27276ca293ac4b0d
Md Abbas Ali <mdabbasali185@gmail.com>
2022-05-13 17:05:34 +0600

    'err'

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -2,13 +2,13 @@ import React from 'react';
 import chair from '../../assets/images/chair.png'
 const Banner = () => {
     return (
-        <div class="hero min-h-screen">
-            <div class="hero-content flex-col lg:flex-row">
-                <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt='banner img' />
+        <div className="hero min-h-screen">
+            <div className="hero-content flex-col flex-reverse lg:flex-row">
+                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='banner img' />
                 <div>
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Get Started</button>
+                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-primary">Get Started</button>
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,12 @@ module.exports = {
         doctor_theme: {
           primary: "#0FCFEC",
           secondary: "#19D3AE",
-          accent: "#3A4256 ",
+          accent: "#3A4256",
           neutral: "#3d4451",
           "base-100": "#ffffff",
         },
       },
-      "dark",
-      "cupcake",
+      
     ],
   },
   plugins: [require("daisyui")],
